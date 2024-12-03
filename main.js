a3d54429abfee9278cb097f82280555787054f4c
leticode <>
2024-12-03 16:25:27 -0300

    termine la primer pre entrega del curso de js

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-let edad = parseInt(prompt("Ingresa tu edad"));
 
-function verPelicula(){
-    if (edad > 18){
-        console.log("podes ingresar a ver la pelicula")
+function comprarEntradas() {
+    const edad = parseInt(prompt("Ingresa tu edad"));
+    if (edad < 18){
+        alert(`No podes ingresar a ver la pelicula`);
+        return;
     }
-    else{
-        console.log("no podes ingresar a ver la pelicula")
+    const precioEntrada = 300;
+    const cantidadEntradas = parseInt(prompt("ingrese la cantidad de entrada que queres"));
+    const total = cantidadEntradas * precioEntrada;
+    for (let i = 0 ; i < cantidadEntradas; i++) {
+        console.log(`imprimiendo entrada ${i + 1}`);
     }
+    console.log(`cantidad de entradas : ${cantidadEntradas}, total : $${total}`);
 }
 
-verPelicula();
+comprarEntradas();
