f5c5fe647b04f3a7fd65d360bf01b60bb99b7d1a
leticode <>
2024-12-05 10:10:32 -0300

    agregue un else

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ function comprarEntradas() {
         alert("No tenes la edad suficiente para ver la pelicula");
         return;
     }
+    else{
+        alert("Tenes la edad suficiente para ver la pelicula");
+    }
     const precioEntrada = 300;
     const cantidadEntrada = parseInt(prompt("Ingresa la cantidad de entradas que necesites"));
     const total = precioEntrada * cantidadEntrada;
-
+    
     for (let i = 0; i < cantidadEntrada; i++){
         console.log(`Imprimiendo entrada ${i + 1}`);
     }
